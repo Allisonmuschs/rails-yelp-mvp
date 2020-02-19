e9a2990c7a94a33b2089974617b011df4a8412ae
Allison Muschs <allisonmuschs@gmail.com>
2020-02-19 12:21:47 +0100

    completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'japanese',
+    phone_number: '04 34 43 04 50'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '04 34 55 22 33',
+    category:  'italian'
+  },
+  {
+    name:         'Champion',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '04 34 55 22 33',
+    category:  'italian'
+  },
+  {
+    name:         'Merlin',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '04 34 55 22 33',
+    category:  'french'
+  },
+  {
+    name:         'La cantina',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '04 34 55 22 33',
+    category:  'italian'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
